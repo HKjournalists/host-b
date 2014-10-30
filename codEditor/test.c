1508d81da38953cdac11fa5178b1902bc630727c
wangbin <inzaghi911911@gmail.com>
2014-10-30 22:12:40 +0800

    add new function : c function replacement

--- codEditor/test.c
+++ codEditor/test.c
@@ -1,11 +1,38 @@
+
+
 #include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int hi();
 
 int main()
 {
     int flag = 0;
     if(flag)
     {
-        printf("Hello world!\n");
+        printf("Hello world!%d,  hi();\n", hi());
     }
+    char *str = "abcdefghi()"
+    char *str = "abcdefg hi()"
+    printf("Hello world!%d,%d,%d,%dashi() sd hi() =hi() +hi();\n", hi(), 1 + hi(), hi(), hi());
+    printf("hi()");
+    printf("hi();");
+    //strcat(hi(), "hi()");
+    //strcat(hi(), "hi();");
+    hi();
+    int i = hi();
+    int j=hi();
+    int m = 3*hi();
+    m += hi();
+    m+=hi();
+    printf(hi());
+    printf("%d", 5+hi());
     return 1;
+}
+
+
+int hi()
+{
+    printf("hi\n");
 }
